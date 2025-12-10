a50aa703cad53c51899fe9cdb4eb46e5e5474091
Parvati <parvathynair186@gmail.com>
2025-12-10 14:03:34 +0530

    'trial'

--- UserApp/serializers.py
+++ UserApp/serializers.py
@@ -7,6 +7,7 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+
     password2 = serializers.CharField(write_only=True, required=True, label="Confirm password")
 
     class Meta:
