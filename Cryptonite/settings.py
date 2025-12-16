217dcea8eb8d94d45f22b50af87f7309470b56f2
Parvati <parvathynair186@gmail.com>
2025-12-16 17:12:26 +0530

    'cloudinary-setup-done'

--- Cryptonite/settings.py
+++ Cryptonite/settings.py
@@ -80,9 +80,17 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "UserApp",
     "AdminApp",
-    'rest_framework_simplejwt.token_blacklist'
+    'rest_framework_simplejwt.token_blacklist',
+    'cloudinary',
+    'cloudinary_storage'
 
 ]
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env("CLOUDINARY_CLOUD_NAME"),
+    'API_KEY': env("CLOUDINARY_API_KEY"),
+    'API_SECRET': env("CLOUDINARY_API_SECRET"),
+}
 
 AUTH_USER_MODEL = "UserApp.User"
 
