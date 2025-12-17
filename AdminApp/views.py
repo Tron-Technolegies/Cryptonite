77b35d0e075eff8c771bb9914b81a3d5d8d238d6
Parvati <parvathynair186@gmail.com>
2025-12-17 17:40:51 +0530

    'fixed-import-issue'

--- AdminApp/views.py
+++ AdminApp/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from AdminApp.models import BundleOffer, Product
-from .serializers import BundleOfferSerializer, ProductSerializer
+from .serializers import BundleOfferSerializer
 
 # ---------- CREATE PRODUCT ----------
 
