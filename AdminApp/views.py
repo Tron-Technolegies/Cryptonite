f2714f86f5fb2df5888f3a5a076df051b228a20a
Parvati <parvathynair186@gmail.com>
2025-12-24 13:58:35 +0530

    'turtle-import-removed'

--- AdminApp/views.py
+++ AdminApp/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from turtle import pd
+# from turtle import pd
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status, permissions
