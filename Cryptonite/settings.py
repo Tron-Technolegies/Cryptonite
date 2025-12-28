b5647a49af0929daa8182613162be1199cf9a356
Parvati <parvathynair186@gmail.com>
2025-12-28 16:11:16 +0530

    'added-https'

--- Cryptonite/settings.py
+++ Cryptonite/settings.py
@@ -60,7 +60,7 @@ CORS_ALLOWED_ORIGINS =[
     "http://localhost:5175",
     "https://cryptonitemining.netlify.app",
     "https://cryptonite-admin.netlify.app",
-    "http://cryptonite.at"
+    "https://cryptonite.at"
     
 ]
 CORS_ALLOW_CREDENTIALS = True
