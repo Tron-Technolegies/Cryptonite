8c8218df3320925a5882a03f4d4bfed8015bbb62
Parvati <parvathynair186@gmail.com>
2026-01-28 11:30:41 +0530

    'import-error-fixed'

--- UserApp/views.py
+++ UserApp/views.py
@@ -78,7 +78,9 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
- 
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
 @method_decorator(csrf_exempt, name="dispatch")
 class ResendVerificationEmailView(APIView):
     permission_classes = [AllowAny]
