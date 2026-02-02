10a3250ab0d0c05209b39c4ba3527344682834f5
Parvati <parvathynair186@gmail.com>
2026-02-02 17:22:58 +0530

    'filters-updated'

--- UserApp/filters.py
+++ UserApp/filters.py
@@ -2,9 +2,23 @@
 import django_filters
 from AdminApp.models import Product
 
+
+# 🔹 enable ?brand=a,b,c
+class CharInFilter(django_filters.BaseInFilter, django_filters.CharFilter):
+    pass
+
+
 class ProductFilter(django_filters.FilterSet):
+    # price range
     min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
     max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+
+    # multi-select
+    brand = CharInFilter(field_name="brand", lookup_expr="in")
+    coin = CharInFilter(field_name="currency", lookup_expr="in")
+
+    # boolean
+    inStock = django_filters.BooleanFilter(field_name="is_available")
 
     class Meta:
         model = Product
